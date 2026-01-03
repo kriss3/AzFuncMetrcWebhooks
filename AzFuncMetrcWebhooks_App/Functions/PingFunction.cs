848d4b079d0a9d01bffa47adf99532eb3f8cb306
Kris <kris_s2@outlook.com>
2026-01-02 23:27:40 -0800

    Add project files.

---
+++ AzFuncMetrcWebhooks_App/Functions/PingFunction.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+public sealed class PingFunction
+{
+	[Function("Ping")]
+	public static async Task<HttpResponseData> Run(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")]
+		HttpRequestData req)
+	{
+		var response = req.CreateResponse(HttpStatusCode.OK);
+		await response.WriteStringAsync("OK - Azure Function Isolated is live");
+		return response;
+	}
+}
