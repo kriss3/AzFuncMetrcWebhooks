53afddd63db46d933a726fb633eb4d65278e9361
Kris <kris_s2@outlook.com>
2026-01-04 12:34:54 -0800

    kata: minor refactor

--- AzFuncMetrcWebhooks_App/Program.cs
+++ AzFuncMetrcWebhooks_App/Program.cs
@@ -1,14 +1,19 @@
+using AzFuncMetrcWebhooks_App.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+	.ConfigureFunctionsWebApplication()
+	.ConfigureServices(services =>
+	{
+		services.AddApplicationInsightsTelemetryWorkerService();
+		services.ConfigureFunctionsApplicationInsights();
+
+		services.AddHttpClient();
+		services.AddSingleton<PushoverNotificationService>();
+		services.AddSingleton<MetrcWebhookValidator>();
+	})
+	.Build();
 
 host.Run();
