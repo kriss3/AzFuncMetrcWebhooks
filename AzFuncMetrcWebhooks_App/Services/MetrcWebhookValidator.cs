9f55c418e5c83f9c8dff2ce39b59e9200d17bdba
Kris <kris_s2@outlook.com>
2026-01-04 12:26:58 -0800

    kata: scaffolding validation function.

--- AzFuncMetrcWebhooks_App/Services/MetrcWebhookValidator.cs
+++ AzFuncMetrcWebhooks_App/Services/MetrcWebhookValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,4 +7,10 @@ namespace AzFuncMetrcWebhooks_App.Services;
 
 public sealed class MetrcWebhookValidator
 {
+	public bool IsValid(HttpRequestData req) 
+	{
+		bool result = false;
+
+		return result;
+	}
 }
