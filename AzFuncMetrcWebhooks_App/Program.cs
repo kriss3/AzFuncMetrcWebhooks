632d0a04d24355bdcf4ef3c8d798c3d490f09c13
Kris <kris_s2@outlook.com>
2026-01-04 21:33:10 -0800

    kata: fixing DI

--- AzFuncMetrcWebhooks_App/Program.cs
+++ AzFuncMetrcWebhooks_App/Program.cs
@@ -6,8 +6,8 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddFunctionsWorkerDefaults();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<PushoverNotificationService>();
 
-builder.Services.AddSingleton<PushoverNotificationService>();
 builder.Services.AddSingleton<MetrcWebhookValidator>();
 
 var host = builder.Build();
