f6142bc87dd75e848cc9fb291f310b3952ca60e7
Kris <kris_s2@outlook.com>
2026-01-22 23:14:34 -0800

    kata: starting again with a better service implementation.

---
+++ AzFuncMetrcWebhooks_App/Services/MetrcPackagesWebhookInspectorService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzFuncMetrcWebhooks_App.Services;
+
+public sealed class MetrcPackagesWebhookInspectorService
+{
+	private readonly MetrcWebhookValidator _validator;
+	private readonly ILogger<MetrcPackagesWebhookInspectorService> _log;
+}
