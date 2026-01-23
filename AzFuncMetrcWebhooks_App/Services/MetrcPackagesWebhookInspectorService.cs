ae7e9a6e17f3b0b32627a5509686b4b6653bf01f
Kris <kris_s2@outlook.com>
2026-01-22 23:14:52 -0800

    kata: adding ctor.

--- AzFuncMetrcWebhooks_App/Services/MetrcPackagesWebhookInspectorService.cs
+++ AzFuncMetrcWebhooks_App/Services/MetrcPackagesWebhookInspectorService.cs
@@ -11,4 +11,12 @@ public sealed class MetrcPackagesWebhookInspectorService
 {
 	private readonly MetrcWebhookValidator _validator;
 	private readonly ILogger<MetrcPackagesWebhookInspectorService> _log;
+
+	public MetrcPackagesWebhookInspectorService(
+		MetrcWebhookValidator validator,
+		ILogger<MetrcPackagesWebhookInspectorService> log)
+	{
+		_validator = validator;
+		_log = log;
+	}
 }
